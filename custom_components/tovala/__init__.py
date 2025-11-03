714f40ac9261ffa2eceb1a2edafa110a596f99c7
Jason Lazerus <jlazerus@infosecured.org>
2025-11-03 15:45:00 -0500

    updates for auth

--- custom_components/tovala/__init__.py
+++ custom_components/tovala/__init__.py
@@ -54,7 +54,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-
+ 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a Tovala config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
